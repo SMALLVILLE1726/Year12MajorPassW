da8448a313334c09aaeb006f355465b3ca80d695
SMALLVILLE1726 <105030129+SMALLVILLE1726@users.noreply.github.com>
2022-05-06 12:41:48 +1000

    Initial commit

---
+++ Assets/PasswordGen.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PasswordGen : MonoBehaviour
+{
+
+    public Text PasswordText;
+    public Slider LengthSlider;
+    public Toggle LettersToggle;
+    public Toggle SymbolsToggle;
+    public Toggle SpacesToggle; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
