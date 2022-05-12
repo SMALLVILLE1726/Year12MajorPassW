35dfc11c948bcc651d27cbd566ad8ac3f87fd652
SMALLVILLE1726 <105030129+SMALLVILLE1726@users.noreply.github.com>
2022-05-12 14:56:09 +1000

    Added Random generator
    Added Letters, Symbols And Numbers.

--- Assets/PasswordGen.cs
+++ Assets/PasswordGen.cs
@@ -15,7 +15,23 @@ public class PasswordGen : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < 15; i++)
+        {
+            int rnd = Random.Range(65, 91);
+            print((char)rnd);
+        }
+
+        for (int i = 0; i < 15; i++)
+        {
+            int rnd = Random.Range(33, 126);
+            print((char)rnd);
+        }
+
+        for (int i = 0; i < 15; i++)
+        {
+            int rnd = Random.Range(48, 57);
+            print((char)rnd);
+        }
     }
 
     // Update is called once per frame
