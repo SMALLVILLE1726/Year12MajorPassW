1dd5149612024482e3bfdf0b9012fc5dbcd4a992
SMALLVILLE1726 <105030129+SMALLVILLE1726@users.noreply.github.com>
2022-06-08 11:50:19 +1000

    Added 'rnd2' and 'added'
    Managed to add rnd2 to randomise the order of the symbols, numbers and letters for the password being formed. The project is more or less finished. But there's a few more touches to add before the project is finished,

--- Assets/PasswordGen.cs
+++ Assets/PasswordGen.cs
@@ -18,29 +18,47 @@ public class PasswordGen : MonoBehaviour
     {
         PW = "";
         // Added a "for" Statement to loop the program
+
         for (int i = 0; i < LengthSlider.value; i++)
         {
             //Added "if" statements to only activate certain variables when turned on.
 
+            //Added "rnd2" to randomise the order of the symbols,numbers and letters. 
+
+            int rnd2 = Random.Range(0, 3);
+           
+            //Added "bool" and "added" to aelect the symbols, numbers and letters seperately.
+
+            bool added = false;
+
             //Characters
-            if (LettersToggle.isOn)
+            if (LettersToggle.isOn && rnd2 == 0)
             {
                 rnd = Random.Range(65, 90);
                 PW += (char)rnd;
+                added = true;
             }
 
             //Symbols 
-            if (SymbolsToggle.isOn)
+            if (SymbolsToggle.isOn && rnd2 == 1)
             {
                 rnd = Random.Range(33, 47);
                 PW += (char)rnd;
+                added = true;
             }
 
             //Numbers
-            if (SpacesToggle.isOn)
+            if (SpacesToggle.isOn && rnd2 == 2)
             {
                 rnd = Random.Range(48, 57);
                 PW += (char)rnd;
+                added = true;
+            }
+
+            if (added == false)
+            {
+                i--;
+                //Added a "i--;" to loop the program again in case of the variables isn't selected. 
             }
         }
 
